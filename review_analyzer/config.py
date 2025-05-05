d2cb5d5b73f44d8ad314058543b2635a0ae5f8e9
Raulinho-A <raul0207abanto@gmail.com>
2025-05-04 20:57:08 -0500

    add data cleaning and preprocessing steps

--- review_analyzer/config.py
+++ review_analyzer/config.py
@@ -15,8 +15,10 @@ RAW = DATA_DIR / "raw"
 
 TIKTOK_COMMENTS = RAW / "comentarios_tiktok_consolidados_1.0.csv"
 
-# CLIPS = PROCESSED / "bboxes" / "clips"
-# AUGMENTED = PROCESSED / "bboxes" / "augmented"
+EXP_SNAPSHOT_PATH = INTERIM / "comentarios_explorados_1.0.csv"
+TRAD_SNAPSHOT_PATH = INTERIM / "comentarios_traducidos_1.0.csv"
+
+BERT_EMBEDDINGS = PROCESSED / "bert_embeddings.npy"
 
 # Models
 MODELS_DIR = PROJ_ROOT / "models"
